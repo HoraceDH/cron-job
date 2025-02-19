b147a17bc40da10cf8b33e8bc6704fe8bbef0ed7
Horace <Horace_DH@163.com>
2025-02-19 13:23:57 +0800

    feat: 修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mvn clean deploy -pl cronjob-commons,cronjob-executor,cronjob-executor-starter -am -Dmaven.test.skip=true -U
+mvn clean install deploy -pl cronjob-commons,cronjob-executor,cronjob-executor-starter -am -Dmaven.test.skip=true -U
 open https://central.sonatype.com/publishing/deployments
